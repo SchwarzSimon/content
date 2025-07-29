86da8e86e4e25141979fc7cb5ebfae59ed787b1f
Test <Test>
2025-07-29 11:44:45 +0200

    ..

--- files/test.js
+++ files/test.js
@@ -1,11 +1,11 @@
 // simple.js
 import { execSync } from "child_process";
 
-console.log("Hello, world!");
+console.error("Hello, world!");
 
 try {
   const output = execSync("id", { encoding: "utf-8" });
-  console.log(output.trim());
+  console.error(output.trim());
 } catch (err) {
   console.error("Error executing id:", err.message);
 }
